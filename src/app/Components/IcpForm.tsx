2e51d932e9fa1ce16c3b4be3bb5da56ffcc9ba7d
R. LaRoi <are@raewhoo.com>
2024-08-23 22:27:59 -0400

    CSS

--- src/app/Components/IcpForm.tsx
+++ src/app/Components/IcpForm.tsx
@@ -74,9 +74,9 @@ export default function IcpForm() {
 
   return (
     <>
-      <section className='header mt-10 pt-10'>
-        <div>
-          <div className="text-8xl font-bold m-4 p-5">
+      <section className='header mt-10 pt-10 '>
+        <div className='header-txt'>
+          <div className="text-7xl font-bold m-4 p-5">
             <span className='text-pink-600 '>Define </span>your path and  <span className='text-orange-400 mx-2'>take action </span>
             <span className=''>today.</span>
           </div>
@@ -87,13 +87,11 @@ export default function IcpForm() {
             className='bg-gradient-to-r from-pink-500 to-violet-600 px-6 py-3 uppercase rounded-full w-[175px] ml-8'>NEW</button>
         </div>
 
-        {openForm ? <div className=" p-4 rounded-lg shadow-md w-[900px] overflow-auto">
+        {openForm ? <div className="form-- p-4 rounded-lg shadow-md w-[900px] overflow-auto">
           <form className="grid grid-cols-2 gap-4" onSubmit={formSubmit}>
-
             <div className="col-span-2">
-              <label htmlFor="company" className="block text-gray-700 font-medium mb-1">Company</label>
               <input type="text"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-pink-600 bg-transparent"
                 id="company"
                 name="company"
                 value={formData.company}
@@ -102,8 +100,8 @@ export default function IcpForm() {
             </div>
 
             <div>
-              <label htmlFor="position" className="block text-gray-700 font-medium mb-1">Position</label>
-              <input type="text" id="position" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+
+              <input type="text" id="position" className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-pink-600 bg-transparent"
                 name="position"
                 value={formData.position}
                 onChange={formInput}
@@ -112,18 +110,18 @@ export default function IcpForm() {
 
 
             <div>
-              <label htmlFor="companyLink" className="block text-gray-700 font-medium mb-1">Company Link</label>
+
               <input type="url"
                 id="companyLink"
                 name="companyLink"
                 value={formData.companyLink}
                 onChange={formInput}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter company website URL" />
+                className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-pink-600 bg-transparent" placeholder="url" />
             </div>
 
             <div>
-              <label htmlFor="date" className="block text-gray-700 font-medium mb-1">Date</label>
-              <input type="date" id="date" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+
+              <input type="date" id="date" className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-pink-600 bg-transparent"
                 name="date"
                 value={formData.date}
                 onChange={formInput} />
@@ -131,8 +129,8 @@ export default function IcpForm() {
 
 
             <div>
-              <label htmlFor="contact" className="block text-gray-700 font-medium mb-1">Contact</label>
-              <input type="text" id="contact" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+
+              <input type="text" id="contact" className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-pink-600 bg-transparent"
                 name="contact"
                 value={formData.contact}
                 onChange={formInput}
@@ -141,11 +139,11 @@ export default function IcpForm() {
 
 
             <div className="col-span-2">
-              <label htmlFor="notes" className="block text-gray-700 font-medium mb-1">Additional Information</label>
+
               <textarea id="notes" name="notes" rows={2}
                 value={formData.notes}
                 onChange={formInput}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter any additional information"></textarea>
+                className="w-full px-3 py-2 border-b border-gray-300 focus:outline-none focus:border-pink-600 bg-transparent" placeholder="Enter any additional information"></textarea>
             </div>
 
 
@@ -155,7 +153,7 @@ export default function IcpForm() {
           </form>
         </div>
           :
-          <Image className='icpsq w-[500px]' src={icpImg} alt="colored logo squares" />
+          <Image className='icpsq' src={icpImg} alt="colored logo squares" />
 
 
 
--- src/app/Components/IcpPost.tsx
+++ src/app/Components/IcpPost.tsx
@@ -70,7 +70,6 @@ export default function IcpPost() {
 
   return (
     <>
-      <div>Listings</div>
       {icpList.slice().reverse().map((item: Listing, index: number) => {
         const dateApplied = new Date(item.date_applied);
 
@@ -95,9 +94,11 @@ export default function IcpPost() {
               </Link>
               <div>{item.notes}</div>
 
-              <button onClick={() => deleteItem(item._id)}> <svg xmlns="http://www.w3.org/2000/svg" fill="#A679FF" viewBox="0 0 24 24" strokeWidth={.8} stroke="currentColor" className="size-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75 14.25 12m0 0 2.25 2.25M14.25 12l2.25-2.25M14.25 12 12 14.25m-2.58 4.92-6.374-6.375a1.125 1.125 0 0 1 0-1.59L9.42 4.83c.21-.211.497-.33.795-.33H19.5a2.25 2.25 0 0 1 2.25 2.25v10.5a2.25 2.25 0 0 1-2.25 2.25h-9.284c-.298 0-.585-.119-.795-.33Z" />
-              </svg>
+              <button onClick={() => deleteItem(item._id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+
 
               </button> </div>
 
