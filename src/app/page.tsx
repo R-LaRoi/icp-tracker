dadaf6cd669580dabebbc3c870690dc78225e9c6
R. LaRoi <are@raewhoo.com>
2024-08-27 20:39:02 -0400

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import IcpForm from "./Components/IcpForm";
 import IcpPost from "./Components/IcpPost";
 import { Nav } from "./Components/Nav";
 
+
 export default function Home() {
-  return (<>
-    <Nav />
-    <IcpForm />
-    <IcpPost />
-  </>)
+  return (
+    <>
+
+      <Nav />
+      <IcpForm />
+      <IcpPost />
+    </>)
 }
